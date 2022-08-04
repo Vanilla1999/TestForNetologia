db2f122903a05b93157318b74838fa14ed793849
Vanilla <vetal-vetal-vv@mail.ru>
2022-08-04 18:18:27 +0300

    add lib for ViewBinding

--- app/src/main/java/com/example/testfornetologia/presentation/HomeFragment.kt
+++ app/src/main/java/com/example/testfornetologia/presentation/HomeFragment.kt
@@ -7,7 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.testfornetologia.R
+import com.example.testfornetologia.databinding.FragmentHomeBinding
 import com.example.testfornetologia.di.mainActivtiy.DaggerHomeFragmentComponent
 import com.example.testfornetologia.di.mainActivtiy.HomeFragmentComponent
 
@@ -16,7 +18,7 @@ import com.example.testfornetologia.di.mainActivtiy.HomeFragmentComponent
  */
 class HomeFragment : Fragment(R.layout.fragment_home) {
     lateinit var homeFragmentComponent: HomeFragmentComponent
-
+    private val binding: FragmentHomeBinding by viewBinding()
 
 
     override fun onAttach(context: Context) {
